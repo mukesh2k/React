8cd45761779c82f5da6f42670695fd0b92fd4e25
Mukesh <mukesh192k@gmail.com>
2021-01-17 15:51:36 +0530

    made components in react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from "react";
 import ReactDom from "react-dom";
-function Great() {
-  return <div>mukessssh</div>;
+import "./index.css";
+function Book() {
+  return (
+    <section className="bookde">
+      <Image></Image>
+      <Image></Image>
+      <Image></Image>
+      <Image></Image>
+      <Image></Image>
+      <Image></Image>
+      <Image></Image>
+    </section>
+  );
 }
-ReactDom.render(<Great />, document.getElementById("root"));
+const Image = () => {
+  return (
+    <article className="books">
+      <img
+        src="https://lh3.googleusercontent.com/-NJcoSThdoh8/X2eWKyZRbXI/AAAAAAABWQo/bx-ZuOHAVNELiFpeumxM1Rp6jV79jm_LgCLcBGAsYHQ/5532ee3f3995482396b513c818f5dc75-g.jpg"
+        alt="mok"
+      ></img>
+      <Name></Name>
+      <How></How>
+    </article>
+  );
+};
+const Name = () => <h2>Life of Monk</h2>;
+const How = () => <p>jay</p>;
+
+ReactDom.render(<Book />, document.getElementById("root"));
